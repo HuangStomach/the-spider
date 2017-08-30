9671864d1741e50c7e22c29e41dc6e313d3b8500
huangStoamch <nxmbest@qq.com>
2017-08-30 12:37:54 +0000

    重命名controller 并增加对server的查询

--- controller/RecordController.php
+++ controller/RecordController.php
@@ -20,13 +20,17 @@ class RecordController extends Controller {
 
         // 对数据做完整性验证
         if (!$record->validation()) {
+            // TODO 错误信息回传待完善
+            // 也需要做log处理
             echo print_r($record->getMessages(), true);
             return;
         }
 
         if ($record->save()) {
+            // TODO 是否对server的设置一定要在这里做? 能否放到异步任务做处理?
             $record->setServer($form);
 
+            // 异步处理任务 发送HTTP的广播以及记录
             $this->server->task([
                 'class' => 'status',
                 'action' => 'http',
